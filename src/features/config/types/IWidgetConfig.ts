5781621821c8b1ab9bd8e06b53711d64f9a682ba
Davasny <davasny@gmail.com>
2025-04-02 17:27:30 +0200

    chore: remove unused rspo config property

--- src/features/config/types/IWidgetConfig.ts
+++ src/features/config/types/IWidgetConfig.ts
@@ -1,7 +1,6 @@
 import * as v from "valibot";
 
 export const ConfigSchema = v.object({
-  rspo: v.optional(v.number()),
   "disable-animations": v.optional(v.boolean(), false),
   "show-branding": v.optional(v.boolean(), true),
   debug: v.optional(v.boolean(), false),
