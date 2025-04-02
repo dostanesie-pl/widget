134b8db5e4db6a09aecd74eaa14c7a580f9e2b3d
Davasny <davasny@gmail.com>
2025-04-02 17:15:47 +0200

    fix(wordpress): escape values in render.php

--- wordpress/src/render.php
+++ wordpress/src/render.php
@@ -1,13 +1,44 @@
 <?php
-// Check if WP_DEBUG is enabled or if debug attribute is set to true
+
+$block_id = wp_json_encode($attributes["blockId"]);
+$disable_animations = wp_json_encode(!$attributes["enableAnimations"]);
+$show_branding = wp_json_encode($attributes["showBranding"]);
+
 $debug_enabled =
     (defined("WP_DEBUG") && WP_DEBUG) ||
     (isset($attributes["debug"]) && $attributes["debug"] === true);
+$debug_enabled = wp_json_encode($debug_enabled);
 
-if ($debug_enabled) {
-    // Replace data-debug="false" with data-debug="true"
-    $content = str_replace('data-debug="false"', 'data-debug="true"', $content);
-}
+$div_content =
+    '
+<div
+  id=' .
+    $block_id .
+    '
+  data-disable-animations="' .
+    $disable_animations .
+    '"
+  data-show-branding="' .
+    $show_branding .
+    '"
+  data-debug="' .
+    $debug_enabled .
+    '"
+></div>
+';
 
-// not using wp_kses_post() because we need to trigger <script> and initialize widget
-echo $content;
+$script_content =
+    '
+<script>
+    window.addEventListener("load", () => {
+      const container = document.getElementById(' .
+    $block_id .
+    ');
+      if (container) {
+        window.loadDostanesiePlWidget?.(container);
+      }
+    });
+</script>
+';
+
+echo $div_content . "\n" . $script_content;
